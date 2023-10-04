9fe061f187e6d9fbd909b08446a063219e40c7b5
Akkessatsu . <raulbragauchoarz@gmail.com>
2023-10-04 23:52:21 +0000

    Adding Activity

---
+++ test.py
@@ -0,0 +1,156 @@
+from instruments import Eload, Equity, PSU
+from time import sleep
+
+
+class Test:
+    
+
+    def __init__(self):
+        self._id = 1000
+        self._name = 'Load Transient'
+        self._description = 'None'
+        self._fm_state = 'START'
+        self._fm_sleep_time = 0.1
+        self._temperature_step = 0
+
+        # Importing modules
+        self.psu = PSU()
+        self.equity = Equity()
+        self.eload = Eload()
+
+
+        # Setting functions
+        self._config_eload()
+        self._config_equity()
+        self._config_psu()
+
+        
+        # Setting temperature list
+        self._temperature = [-10.0, 25.0, 85.0]
+
+    def _set_voltage(self, voltage):
+        assert (type(voltage) is float) or (type(voltage) is int)
+        if not 0 <= voltage <= 300:
+            raise ValueError('Voltage range error')
+
+        self.psu.set_voltage(voltage)
+
+    def _set_output_current(self, current):
+        assert (type(current) is float) or (type(current) is int)
+        if not 0 <= current <= 5:
+            raise ValueError('Current range error')
+
+        self.eload.write(f'CURR {current}')
+
+    def _set_temperature(self, temperature):
+        assert (type(temperature) is float) or (type(temperature) is int)
+        if not -20 <= temperature <= 100:
+            raise ValueError('Temperature range error')
+        self.equity.set_temperature(temperature)
+
+    def _fm(self):
+        if self._fm_state == 'START':
+            self._current_temperature = self._temperature[self._temperature_step]
+            if not -10 <= self._current_temperature <= 85:
+                raise ValueError('Temperature range error')
+
+            self._stabilization_temperature_time = 40 * 60
+            if not 0 <= self._stabilization_temperature_time <= 3600:
+                raise ValueError('Stabilization temperature time range error')
+
+            self._voltage_C = 120.0
+            
+            self._initial_electric_current = 3.0
+            if not 0 <= self._initial_electric_current <= 5:
+                raise ValueError('Initial electric current range error')
+            assert type(self._initial_electric_current) is float
+
+            self._final_electric_current = 6.0
+            if not 1 <= self._final_electric_current <= 10:
+                raise ValueError('Final electric current range error')
+            assert type(self._final_electric_current) is float
+
+            self._voltage_X = 20.0
+
+            self.eload.write(f'VOLT {self._voltage_X}')
+            self._fm_state = 'CONFIG_EQUITY'
+
+        elif self._fm_state == 'CONFIG_EQUITY':
+            self.equity.set_temperature(self._current_temperature)
+            while self._current_temperature != self.equity.get_temperature():
+                sleep(1)
+            self._fm_state = 'CONFIG_PSU'
+
+        elif self._fm_state == 'CONFIG_PSU':
+
+            self.psu.set_voltage(self._voltage_X)
+            sleep(1)
+            self._fm_state = 'CONFIG_ELOAD'
+
+        elif self._fm_state == 'CONFIG_ELOAD':
+            self.eload.write(f'CURR {self._initial_electric_current}')
+            sleep(1)
+            self._fm_state = 'SHOW_OUTPUT'
+
+        elif self._fm_state == 'SHOW_OUTPUT':
+            self._output_power = float(self.eload.query("MEAS:POW?"))
+
+            print(f'ELOAD Actual electric current: {self.eload.query("MEAS:CURR?")}')
+            print(f'ELOAD Actual electric voltage: {self.eload.query("MEAS:VOLT?")}')
+
+            print(f'PSU Actual electric current: {self.psu.get_current()}')
+            print(f'PSU Actual electric voltage: {self.psu.get_voltage()}')
+
+            print(f'ELOAD Actual output power: {self._output_power}')
+
+            if self._output_power >= self._voltage_X:
+                self._fm_state = 'STEP_COMPLETE'
+            else:
+                self._fm_state = 'CONFIG_ELOAD'
+
+        elif self._fm_state == 'STEP_COMPLETE':
+
+            if self._current_temperature == -10:
+                self._fm_state = 'END'
+            else:
+                self._fm_state = 'VERIFY_TEMPERATURE_STEP'
+
+        elif self._fm_state == 'VERIFY_TEMPERATURE_STEP':
+            self._temperature_step += 1
+            if self._temperature_step < len(self._temperature):
+                self._current_temperature = self._temperature[self._temperature_step]
+                self._fm_state = 'START'
+            else:
+                self._fm_state = 'END'
+
+        elif self._fm_state == 'END':
+            self.psu.set_voltage(0)
+            self.eload.write('CURR 0')
+            self.eload.write('VOLT 0')
+            self._stop_flag = True
+
+        else:
+            raise Exception('Invalid FM state')
+
+    def _config_eload(self):
+        self.eload.write('CURR 0')
+
+    def _config_equity(self):
+        self.equity.set_temperature(25.0)
+
+    def _config_psu(self):
+        self.psu.set_voltage(0.0)
+
+    def _generate_report(self):
+        pass
+
+    def _main(self):
+        self._stop_flag = False
+        while self._stop_flag is False:
+            self._fsm()
+            sleep(self._fsm_sleep_time)
+        print('Test is done')
+
+if __name__ == '__main__':
+    test = Test()
+    test._main()
